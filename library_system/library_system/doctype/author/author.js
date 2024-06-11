925e8a8b51cc2e317ee122c3b213fdef7b5f1275
Farthy <senelwafarthy@gmail.com>
2024-06-11 16:59:49 +0300

    changes

--- library_system/library_system/doctype/author/author.js
+++ library_system/library_system/doctype/author/author.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2024, Farthy and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Author", {
-// 	refresh(frm) {
+frappe.ui.form.on("Author", {
 
-// 	},
-// });
+    validate: function(frm) {
+        if (frm.doc.first_name && frm.doc.last_name) {
+            frm.set_value('full_name', frm.doc.first_name + ' ' + frm.doc.last_name);
+        }
+    }
+});
